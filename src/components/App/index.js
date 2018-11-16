1594bc8b16eb071b09fa6df6233a6dbc48ec1e43
Bojana <bojana.karakacev@gmail.com>
2018-11-16 02:12:50 +0100

    Connect book form handler and list books

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -15,6 +15,7 @@ class App extends Component {
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(e, name) {
@@ -22,19 +23,50 @@ class App extends Component {
   }
  
   handleCheckboxChange() {
-    this.setState((prevState) => ({isRead: !prevState.isRead}));
+    this.setState((prevState) => ({ isRead: !prevState.isRead }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const { books, bookName, authorName, numOfPages, isRead } = this.state;
+    const newBook = { bookName, authorName, numOfPages, isRead };
+
+    books.push(newBook);
+
+    this.setState({ 
+      books,
+      bookName: '', 
+      authorName: '',
+      numOfPages: 0, 
+      isRead: false
+    });
+
+    alert('You successfully added a new book!');
   }
 
   render() {
     return (
       <div>
         <h1>Library</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type="text" name="bookName" value={this.state.bookName} onChange={(e) => this.handleInputChange(e, 'bookName')} required />
           <input type="text" name="authorName" value={this.state.authorName} onChange={(e) => this.handleInputChange(e, 'authorName')} required />
           <input type="number" name="numOfPages" value={this.state.numOfPages} onChange={(e) => this.handleInputChange(e, 'numOfPages')} required />
           <input type="checkbox" name="read" checked={this.state.isRead} onChange={this.handleCheckboxChange} />
+          <button type="submit">Save Book</button>
         </form>
+
+        <ul>
+          {this.state.books.map(book => (
+            <li>
+              Book Name: {book.bookName} <br/>
+              Author Name: {book.authorName} <br/>
+              Number of pages: {book.numOfPages} <br/>
+              Is read?: {book.isRead ? 'Yes' : 'No'} <br/>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
